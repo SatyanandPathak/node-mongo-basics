5359cda11f37a8a1b33502aeec96b7b6ccd31f98
Satyanand Pathak <satyanand.pathak@rackspace.com>
2018-07-25 16:30:08 -0500

    adding code for GET todos by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 
 var app = express();
@@ -15,7 +16,7 @@ app.get('/', (request, response) => {
     response.send('Home Page.');
 });
 
-
+// Get all Todos
 app.get('/todos', (request, response) => {
 
     Todo.find()
@@ -28,6 +29,27 @@ app.get('/todos', (request, response) => {
     });
 });
 
+// Get a TODO by Id
+app.get('/todos/:id', (request, response) => {
+
+    const id = request.params.id;
+    if(!ObjectID.isValid(id)){
+        return response.status(400).send({message: 'Id not valid'});
+    }
+    Todo.findById(id)
+    .then((todo) => {
+        if(!todo){
+            return response.status(404).send({message: 'Could not find a todo'});
+        }
+
+        response.status(200).send({todo});
+    })
+    .catch(e => response.status(500).send({'message':'Internal Server Error'}));
+    
+})
+
+
+// POST a TODO
 app.post('/todos', (request, response) => {
     console.log(request.body);
     var todo = new Todo({
