6b5fe2ad020c8be65e463c4382608cac8b8d15ea
Satyanand Pathak <satyanand.pathak@rackspace.com>
2018-07-25 21:19:07 -0500

    correcting the process value

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
