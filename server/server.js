ab99d044d56730dacba1f614c4fa84ecfdfa16e9
Satyanand Pathak <satyanand.pathak@rackspace.com>
2018-07-28 08:49:50 -0500

    added fetch/put API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -80,6 +81,34 @@ app.delete('/todos/:id', (request, response)=> {
     })
     .catch(e => response.status(500).send(e));
 })
+
+// Update todo. Can use PUT or PATCH
+app.patch('/todos/:id', (request, response) => {
+    var id = request.params.id;
+    // No need to pick all the invalid content that is not supported for update
+    var body = _.pick(request.body, ['completed', 'text'])
+
+    if(!ObjectID(id)){
+        return response.status(404).send({description: 'Id format not supported'});
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(new ObjectID(id), {$set: body}, {new: true})
+    .then((todo) => {
+        if(!todo){
+            return response.status(404).send({description: 'Id not found'});
+        }
+        return response.status(204).send(todo);
+    })
+    .catch(e => response.status(500).send({description: 'Internal Server Error'}));
+
+    
+});
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
